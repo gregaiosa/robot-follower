505822d6c93752ad1b08206f3f5fb46b6e0bc4fb
gaiosa <gregory.aiosa@gmail.com>
2026-01-30 12:15:36 -0600

    Created launchfile to launch nav2 and slam demos along with pointcloud conversion

--- robot-follower/setup.py
+++ robot-follower/setup.py
@@ -1,4 +1,19 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+def recursive_files(prefix, path):
+    """
+    Recurse over path returning a list of tuples suitable for use with setuptools data_files.
+
+    :param prefix: prefix path to prepend to the path
+    :param path: Path to directory to recurse. Path should not have a trailing '/'
+    :return: List of tuples. First element of each tuple is destination path,
+        second element is a list of files to copy to that path
+    """
+    return [(str(Path(prefix)/subdir),
+            [str(file) for file in subdir.glob('*') if not file.is_dir()])
+            for subdir in Path(path).glob('**')]
 
 package_name = 'robot-follower'
 
@@ -10,6 +25,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        *recursive_files('share/' + package_name, 'launch'),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
